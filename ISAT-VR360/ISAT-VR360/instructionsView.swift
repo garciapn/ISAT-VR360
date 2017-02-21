4050f19827afaff195c30aa944c016740b65515b
garciapn <paolo.niko.garcia@gmail.com>
2017-02-21 15:53:04 -0500

    update to local ip

---
+++ ISAT-VR360/ISAT-VR360/instructionsView.swift
@@ -0,0 +1,26 @@
+//
+//  instructionsView.swift
+//  ISAT-VR360
+//
+//  Created by Paolo Garcia  on 2/21/17.
+//  Copyright © 2017 PNGapps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class instructionsView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
